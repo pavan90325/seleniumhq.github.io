8f6e39446f1d2527f0d6a02eefc09bdf7a284597
pavan90325 <pavankalyan90325@gmail.com>
2025-08-14 10:05:37 +0530

    Update FirstScript.java

--- examples/java/src/test/java/dev/selenium/getting_started/FirstScript.java
+++ examples/java/src/test/java/dev/selenium/getting_started/FirstScript.java
@@ -11,21 +11,8 @@ public class FirstScript {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.get("https://www.flipkart.com/");
 
-        driver.getTitle();
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        textBox.sendKeys("Selenium");
-        submitButton.click();
-
-        WebElement message = driver.findElement(By.id("message"));
-        message.getText();
-
-        driver.quit();
+       
     }
 }
